20e495fb162d906ef7e83867a9abfd623510a18e
Aman Rawat <154727782+amanrawatdun@users.noreply.github.com>
2025-08-10 19:26:12 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:import.https://vastraaa-backend.onrender.com
+    baseURL:https://vastraaa-backend.onrender.com
 })
 
 api.interceptors.request.use((data)=>{

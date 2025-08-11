4362602ca8b79ad962dbf66196c22adfac5cb892
Aman Rawat <154727782+amanrawatdun@users.noreply.github.com>
2025-08-11 11:44:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,27 +7,50 @@ const productRouter = require('./routes/productRoutes');
 const orderRouter = require('./routes/orderRoutes');
 const paymentRouter = require('./routes/paymentRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const cluster = require('cluster');
+const os = require('os');
 
 dotenv.config();
+
+
 connectDB();
 
-const app = express();
-app.use(cors({
-    origin:'https://vastraaa-4ti1.onrender.com',
-    Credential:true
-}));
+const numCPUs = os.cpus().length;
 
-app.use(express.json());
-app.use(express.urlencoded(false))
-app.use('/uploads',express.static('uploads'));
+if (cluster.isMaster) {
+    console.log(`Master process ${process.pid} is running`);
+    console.log(`Starting ${numCPUs} workers...`);
 
+    
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
+   
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Starting a new one...`);
+        cluster.fork();
+    });
 
-app.use('/api/users' , userRouter)
-app.use('/api/products' , productRouter)
-app.use('/api/orders' , orderRouter);
-app.use('/api/payments', paymentRouter);
-app.use('/api/admin' , adminRoutes);
+} else {
+    
+    const app = express();
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT , ()=>console.log(`Server running on port ${PORT}`));
+    app.use(cors({
+        origin: 'https://vastraaa-4ti1.onrender.com',
+        credentials: true 
+    }));
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use('/uploads', express.static('uploads'));
+
+    app.use('/api/users', userRouter);
+    app.use('/api/products', productRouter);
+    app.use('/api/orders', orderRouter);
+    app.use('/api/payments', paymentRouter);
+    app.use('/api/admin', adminRoutes);
+
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Worker ${process.pid} running on port ${PORT}`));
+}

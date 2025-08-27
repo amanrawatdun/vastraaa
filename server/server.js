045d16dd3b49e8214d98514ffa7eac61150f5c01
Aman Rawat <154727782+amanrawatdun@users.noreply.github.com>
2025-08-27 13:14:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ const cluster = require('cluster');
 const os = require('os');
 
 dotenv.config();
-
-
 connectDB();
 
 const numCPUs = os.cpus().length;
@@ -21,29 +19,35 @@ if (cluster.isMaster) {
     console.log(`Master process ${process.pid} is running`);
     console.log(`Starting ${numCPUs} workers...`);
 
-    
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
 
-   
     cluster.on('exit', (worker, code, signal) => {
         console.log(`Worker ${worker.process.pid} died. Starting a new one...`);
         cluster.fork();
     });
 
 } else {
-    
     const app = express();
 
     app.use(cors({
         origin: 'https://vastraaa-4ti1.onrender.com',
-        credentials: true 
+        credentials: true
     }));
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use('/uploads', express.static('uploads'));
+
+   
+    app.get('/health', (req, res) => {
+        res.set('Cache-Control', 'no-store');
+        res.status(200).send('OK');
+    });
+
+  
+    app.head('/health', (req, res) => res.sendStatus(200));
 
     app.use('/api/users', userRouter);
     app.use('/api/products', productRouter);
